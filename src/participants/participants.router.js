700dbc211c5213c09559f3d61e028f03c752ffe4
Jose Carrero <carrechez1@gmail.com>
2022-10-26 17:55:37 -0500

    V1.0.8

--- src/participants/participants.router.js
+++ src/participants/participants.router.js
@@ -1,11 +1,14 @@
-const router = require('express').Router()
-const passport = require('passport')
+const router = require("express").Router();
+const passport = require("passport");
 
-const participantsServices = require('./participants.services')
-require('../middlewares/auth.middleware')(passport)
+const participantsServices = require("./participants.services");
+require("../middlewares/auth.middleware")(passport);
 
-router.route('/')
-    .get(
-        passport.authenticate('jwt', {session: false}),
-        participantsServices.getAllParticipants
-        )
+router
+  .route("/")
+  .get(
+    passport.authenticate("jwt", { session: false }),
+    participantsServices.getAllParticipants
+  );
+
+module.exports = router;
